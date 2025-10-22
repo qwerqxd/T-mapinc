52b78a45ee5159df59c0b49fa06dd87f2ff9c6f7
Novsistem <novs9532@gmail.com>
2025-10-22 15:31:10 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   output: 'standalone',
   // Adding a comment to force a change and cache invalidation.
+  devServer: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1761068031576.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev',
+      'https://9000-firebase-studio-1761068031576.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev',
+    ],
+  },
 };
 
 module.exports = nextConfig;
