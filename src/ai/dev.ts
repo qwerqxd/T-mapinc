8382d68c41eed49ea89c9a29baa97eea48897e5a
Novsistem <novs9532@gmail.com>
2025-10-22 22:18:03 +0000

    удали google map api Failed to compile. 01:15:24 | DEBUG | #7 55.60 01:

--- src/ai/dev.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-// This file is used to import flows for their side effects in development.
-// In production, this file should be empty.
-import { config } from 'dotenv';
-
-config();
-
-// Import flows here for their side effects in development.
-// import './flows/get-location-from-coords-flow';
