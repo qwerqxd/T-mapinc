6cd6111cb6321d3865d8e92608d4d997ed647e4a
Ulyan Sofhob <132189586+kazlica@users.noreply.github.com>
2025-10-27 23:34:56 +0300

    updated dependencies

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     DB_URL: string
+    SENTRY_AUTH_TOKEN: string
+    YMAPS_API: string
   }
 }
--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,6 @@
-import postgres from "postgres"
+import { config } from "dotenv"
 import { drizzle } from "drizzle-orm/postgres-js"
-import "dotenv/config"
+import postgres from "postgres"
 
 async function main() {
   const client = postgres(process.env.DB_URL, { prepare: false })
