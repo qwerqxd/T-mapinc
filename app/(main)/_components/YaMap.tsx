063095e218ddf270d584574ac8036b19635ba6cd
Ulyan Sofhob <132189586+kazlica@users.noreply.github.com>
2025-11-04 04:52:21 +0300

    added theme colors

--- app/(main)/_components/YaMap.tsx
+++ app/(main)/_components/YaMap.tsx
@@ -1,6 +1,13 @@
-// import { Map } from "@iminside/react-yandex-maps"
-// import type { MapProps } from "@/types"
+import { Map } from "@iminside/react-yandex-maps"
+import type { MapProps } from "@/types"
 
-// export default function YaMap(props: MapProps) {
-//   return <Map {...props} />
-// }
+export default function YaMap(props: MapProps) {
+  return (
+    <Map
+      defaultState={{ center: [55.75, 37.57], zoom: 9 }}
+      width="100vw"
+      height="100vh"
+      {...props}
+    />
+  )
+}
